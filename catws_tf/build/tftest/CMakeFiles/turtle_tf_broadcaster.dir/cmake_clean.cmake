cffe1da86488d12c8ba240fe3f1267745e698d76
Ravi kiran <bhagsukraj@gmail.com>
2018-05-13 21:28:32 +0530

    update

---
+++ catws_tf/build/tftest/CMakeFiles/turtle_tf_broadcaster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtle_tf_broadcaster.dir/src/turtle_tf_broadcaster.cpp.o"
+  "/home/ravi/robotics2/catws_tf/devel/.private/tftest/lib/tftest/turtle_tf_broadcaster.pdb"
+  "/home/ravi/robotics2/catws_tf/devel/.private/tftest/lib/tftest/turtle_tf_broadcaster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtle_tf_broadcaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- catws_tf/build/tftest/catkin_generated/setup_cached.sh
+++ catws_tf/build/tftest/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ravi/robotics2/catws_tf/devel/.private/tftest:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ravi/robotics2/catws_tf/devel/.private/tftest/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ravi/robotics2/catws_tf/devel/.private/tftest/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/ravi/robotics2/catws_tf/build/tftest"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/ravi/robotics2/catws_tf/devel/.private/tftest/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/ravi/robotics2/catws_tf/src/tftest:$ROS_PACKAGE_PATH"
